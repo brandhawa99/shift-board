4396775d65f3fa8dc33006c4a06b05b58fbd4d17
brandhawa99 <baltej.randhawa0@gmail.com>
2025-12-29 23:12:35 -0800

    feat: add color variant of health checklist logo and update header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { MoveRight } from "lucide-react";
 import { Link, useLocation } from "@tanstack/react-router";
 import logo from "../images/health-checklist-svgrepo-com.svg"
+import logoColour from "../images/health-checklist-svgrepo-com-colour.svg"
 import { Button } from "./ui/button";
 import { Container } from "./Container";
 
@@ -11,26 +12,34 @@ export default function Header() {
 
 
   return (
-    <Container className="flex items-center justify-between py-6" >
-      <Link to={"/"}>
-        <h1 className="max-md:ml-4 text-xl font-semibold flex items-center">
-          Health Board
-          <img src={logo} alt="logo" className="h-8 w-8 ml-2" />
-        </h1>
-      </Link>
-      <div className="flex gap-2">
-        <div className="flex items">
-          <div className="flex gap-2 items-center text-md">
-            Click Here
-            <MoveRight color="#000000" strokeWidth={2} />
+    <div className={`${!isDashboard ? "" : "bg-blue-200"}`} >
+      <Container className="flex items-center justify-between py-6" >
+        <Link to={"/"}>
+          <h1 className="max-md:ml-4 text-xl font-semibold flex items-center">
+            Health Board
+            {
+              !isDashboard ? (
+                <img src={logo} alt="logo" className="h-8 w-8 ml-2 fill-current" />
+              ) : (
+                <img src={logoColour} alt="logo" className="h-8 w-8 ml-2 fill-current" />
+              )
+            }
+          </h1>
+        </Link>
+        <div className="flex gap-2">
+          <div className="flex items">
+            <div className="flex gap-2 items-center text-md">
+              Click Here
+              <MoveRight color="#000000" strokeWidth={2} />
+            </div>
           </div>
+          <Button>
+            <Link to={isDashboard ? "/" : "/dashboard"}>
+              {isDashboard ? "Home" : "Dashboard"}
+            </Link>
+          </Button>
         </div>
-        <Button>
-          <Link to={isDashboard ? "/" : "/dashboard"}>
-            {isDashboard ? "Home" : "Dashboard"}
-          </Link>
-        </Button>
-      </div>
-    </Container >
+      </Container >
+    </div >
   )
 }
